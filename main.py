1f859dc1dc2cf40d4a5d192d591e1dc00cedef7b
DeibyVargas <deibyvargaschaves@gmail.com>
2023-01-13 22:48:32 -0600

    Initial commit

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ lista_paises = paises.split(",")
 pais_unico = set(lista_paises)
 paises_ordenado = sorted(lista_paises)
 print(", ".join(paises_ordenado))
+
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+impares = list(filter(lambda x: x % 2 != 0, numeros))
+print(impares)
